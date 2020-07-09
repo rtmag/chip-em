2eea5566da19a7d781a0ffe5564bbe8609d70475
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-07-10 02:39:09 +0800

    Create preprocessing_encode.bash

---
+++ preprocessing_encode.bash
@@ -0,0 +1,59 @@
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_CEBPB_rep1_ENCFF000OYS.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_CEBPB_rep2_ENCFF000OYU.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --paired --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Bernstein_isogenic1_rep1_R1_ENCFF088BWP.fastq.gz \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Bernstein_isogenic1_rep1_R2_ENCFF804MJI.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --paired --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Bernstein_isogenic1_rep2_R1_ENCFF924HDE.fastq.gz \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Bernstein_isogenic1_rep2_R2_ENCFF993JWW.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Bernstein_isogenic2_rep1_ENCFF973AFN.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Bernstein_isogenic2_rep2_ENCFF564VKX.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Bernstein_isogenic2_rep3_ENCFF126DXV.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Peggy_rep1_ENCFF000VCI.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_H3K4me1_Peggy_rep2_ENCFF000VCK.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --paired --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Bernstein_isogenic1_R1_ENCFF048ZOQ.fastq.gz \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Bernstein_isogenic1_R2_ENCFF827YXC.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Bernstein_isogenic2_rep1_ENCFF413RQG.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Bernstein_isogenic2_rep2_ENCFF796ERZ.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Bernstein_isogenic2_rep3_ENCFF957PGR.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Myers_rep1_ENCFF000PBW.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Myers_rep2_ENCFF000PBY.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Peggy_rep1_ENCFF000VCY.fastq.gz
+
+/home/rtm/myprograms/TrimGalore/trim_galore --illumina -q 20 --fastqc -o /home/rtm/chip-em/encode_data/encode_fastq_trim \
+/home/rtm/chip-em/encode_data/ENCODE_CHIP_EM_CONTROL/HCT116_input_Peggy_rep2_ENCFF000VCW.fastq.gz
+####
+
+
+
+
+

7f66ce94effb02a4bb97ae82a277dec8540867b8
Roberto Tirado Magallanes <rtmag4@gmail.com>
2020-09-03 06:57:19 +0800

    Update chip_correlations.bash

--- chip_correlations.bash
+++ chip_correlations.bash
@@ -50,6 +50,29 @@ plotCorrelation \
     --plotTitle "" \
     --whatToPlot heatmap --colorMap RdYlBu --plotNumbers \
     -o multiBigwigSummary_bin1K.png 
+    
+    #################################################################
+multiBigwigSummary bins -b \
+/home/rtm/chip-em/bam/HCT116_DKO_H3K4me1.bw \
+/home/rtm/chip-em/bam/HCT116_WT_H3K4me1.bw \
+/home/rtm/chip-em/encode_data/encode_bam/HCT116_H3K4me1_Bernstein_isogenic1_rep1.bw \
+/home/rtm/chip-em/encode_data/encode_bam/HCT116_H3K4me1_Bernstein_isogenic1_rep2.bw \
+/home/rtm/chip-em/encode_data/encode_bam/HCT116_H3K4me1_Bernstein_isogenic2_rep1.bw \
+/home/rtm/chip-em/encode_data/encode_bam/HCT116_H3K4me1_Bernstein_isogenic2_rep2.bw \
+/home/rtm/chip-em/encode_data/encode_bam/HCT116_H3K4me1_Bernstein_isogenic2_rep3.bw \
+/home/rtm/chip-em/encode_data/encode_bam/HCT116_H3K4me1_Peggy_rep1.bw \
+/home/rtm/chip-em/encode_data/encode_bam/HCT116_H3K4me1_Peggy_rep2.bw \
+--labels CHIPEM_DKO_H3K4me1 CHIPEM_WT_H3K4me1 \
+Bernstein_H3K4me1_iso1_rep1 Bernstein_H3K4me1_iso1_rep2 Bernstein_H3K4me1_iso2_rep1 Bernstein_H3K4me1_iso2_rep2 Bernstein_H3K4me1_iso2_rep3 \
+Peggy_H3K4me1_rep1 Peggy_H3K4me1_rep2 \
+-o /home/rtm/chip-em/chip_comparison/multiBigwigSummary_HCT116_H3K4me1_bin_default.npz
+
+plotCorrelation \
+    -in multiBigwigSummary_HCT116_H3K4me1_bin_default.npz \
+    --corMethod spearman --skipZeros \
+    --plotTitle "" \
+    --whatToPlot heatmap --colorMap RdYlBu --plotNumbers \
+    -o multiBigwigSummary_HCT116_H3K4me1_bin_default.png 
 ################################################################################################################################################################
 ################################################################################################################################################################
 computeMatrix reference-point \
